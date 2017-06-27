e670f3e395634d30c128ebe2bce92ad85c11bf87
Anthony Ladson <aladsonjr@gmail.com>
2017-06-27 16:50:18 -0500

    index html page ready

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const http = require('http');
 const fs = require('fs');
 
 const app = express();
-// let html = fs.readFileSync('index.html');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.render('index.html');
+  res.render('index');
 });
 
 http.createServer(app).listen(3000, function(){
